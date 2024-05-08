ee10a8240b17d33e73bb395f0d73c309097e426e
Alina Makarenko <alinamakarenko1306@gmail.com>
2024-05-08 15:30:21 -0500

    almost finished

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-// Create two variables, firstCard and secondCard & Set their values to a random number between 2-11
-let firstCard = 10;
-let secondCard = 11;
-
-// Create a variable, sum, and set it to the sum of the two cards
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard]; // ordered list of items
 let sum = firstCard + secondCard;
 
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
-let message = ""; // Declare a variable called message and assign its value to an empty string
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if(sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if(sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+function startGame() {
+    renderGame();
+}
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for(let i = 0; i < cards.length; i ++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+    
+    sumEl.textContent = "Sum: " + sum;
+    if(sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if(sum === 21) {
+        message = "Wohoo! You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
 }
 
-// console.log(hasBlackJack);
-// console.log(isAlive);
-console.log(message);
+function newCard() {
+    console.log("Drawing a new card from the deck!");
+    let card = getRandomCard();
+    cards.push(card); // add to the end of array
+    sum += card;
+    renderGame();
+}
+
+function getRandomCard() {
+    return 5;
+}
